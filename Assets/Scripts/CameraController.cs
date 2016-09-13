5b1d880b207188ecfd53f58e73097e9847c21eaa
Timothy Forste <tforste@gmail.com>
2016-09-12 23:17:53 -0700

    Camera Controller and Player Controller
    Added movement based on arrow keys by applying forces to the regid body
    of the player and locked the camera into a thrid person view.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate gaurentees that everything in Update has run. By
+    // Performing the camera move in LateUpdate, we know for sure the player
+    // have been moved for that frame.
+	void LateUpdate () {
+	    transform.position = player.transform.position + offset;
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    //Called before rendering a frame
+    //Where much of our game code will go
     void Update()
     {
 
     }
 
+    public float speed;
+    private Rigidbody rb;
+
+    //Called on the first frame, often first frame of the game
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Called just before performing any physics calculations
+    //Where our phsics cade will go
+    //Moving ball by applying forces to the rigid body, this is physics
     void FixedUpdate()
     {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
     }
 }
