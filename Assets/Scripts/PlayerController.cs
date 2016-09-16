3d43a7f2c1ab91636395992c41766fcf1171e5f6
Timothy Forste <tforste@gmail.com>
2016-09-15 22:20:33 -0700

    Dispaying Score and Win Text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
@@ -11,12 +12,19 @@ public class PlayerController : MonoBehaviour {
     }
 
     public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
+    private int count;
 
     //Called on the first frame, often first frame of the game
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = string.Empty;
     }
 
     //Called just before performing any physics calculations
@@ -38,6 +46,16 @@ public class PlayerController : MonoBehaviour {
        if(other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 12)
+            winText.text = "You Win!";
     }
 }
